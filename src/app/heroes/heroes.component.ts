f65b835be37c35196837a4d9d9fb0050e96ed61a
qixiaoling <x.qi@novi-education.nl>
2022-03-30 10:57:01 +0200

    third commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from "../hero";
+import {HEROES} from "../mock-heros";
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Big Boy';
+
+  heroes = HEROES;
+  selectedHero?: Hero;
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
   }
+
 
 }
