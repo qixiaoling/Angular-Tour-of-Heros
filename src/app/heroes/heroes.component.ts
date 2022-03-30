c08e1bd0d2d0ded2ed9510e33a61452b11908013
qixiaoling <x.qi@novi-education.nl>
2022-03-30 11:28:38 +0200

    one object

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
 import {HEROES} from "../mock-heros";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -10,17 +11,27 @@ import {HEROES} from "../mock-heros";
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
 
-  constructor() { }
+  constructor(private heroSerivce: HeroService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     console.log(this.selectedHero);
+  }
+  getHeroes(): void {
+    // this.heroes = this.heroSerivce.getHeroes();
+    this.heroSerivce.getHeroes()
+      .subscribe(x => {
+        console.log(x);
+        this.heroes = x;
+      })
   }
 
 
